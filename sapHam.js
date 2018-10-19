8b8e11e6b04c65fa6d4202d6c8fbd939e6370a30
congkhuong <congkhuong1308@gmail.com>
2018-10-19 17:16:01 +0700

    2

--- sapHam.js
+++ sapHam.js
@@ -1,5 +1,7 @@
 var io;
 var gameSocket;
+var rooms = {};
+var defaultRoom = '100';
 
 /**
  * This function is called by index.js to initialize a new game instance.
@@ -14,10 +16,13 @@ exports.initGame = function(sio, socket){
 
     // Host Events
     gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+    gameSocket.on('roomInfo', roomInfo);
     /*gameSocket.on('hostRoomFull', hostPrepareGame);
     gameSocket.on('hostCountdownFinished', hostStartGame);
     gameSocket.on('hostNextRound', hostNextRound);*/
 
+    // Room event
+    gameSocket.on('roomInfo', roomInfo);
     // Player Events
     gameSocket.on('playerJoinGame', playerJoinGame);
     /*gameSocket.on('playerAnswer', playerAnswer);
@@ -25,34 +30,44 @@ exports.initGame = function(sio, socket){
 }
 
 function hostCreateNewGame() {
+    var sock = this;
     // Create a unique Socket.IO Room
     //const thisGameId = ( Math.random() * 100000 ) | 0;
-    const thisGameId = '100';
+    const thisGameId = defaultRoom;
 
     // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
-    this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+    this.emit('newGameCreated', { gameId: thisGameId, mySocketId: this.id });
 
     // Join the Room and wait for the players
-    this.join(thisGameId.toString());
+    this.join(thisGameId.toString(), () => {
+      rooms[defaultRoom] = {
+        _id: defaultRoom,
+        name: 'Sap Ham',
+        players: [{ username: 'Host', sid: sock.id }],
+      }
+      //console.log(rooms);
+      //console.log(io.nsps['/'].adapter.rooms['100']);
+    });
 };
-
 
 function playerJoinGame(data) {
     // A reference to the player's Socket.IO socket object
     var sock = this;
-
+    //sock.set('username', data.username);   
     // Look up the room ID in the Socket.IO manager object.
     //var room = gameSocket.manager.rooms["/" + data.gameId];
 
+    console.log(io.nsps['/'].adapter.rooms['100']);
     sock.join('100', () => {
-      let rooms = Object.keys(gameSocket.rooms);
-        console.log(io.nsps['/'].adapter.rooms['100']);
+      const playerObj = { username: data.username, sid: sock.id  };
+      rooms['100'].players.push(playerObj);
+      io.sockets.to(defaultRoom).emit('playerJoinedRoom', data);
+      //console.log(rooms);
     });
     //io.sockets.broadcast.emit('playerJoinedRoom', data);
     //console.log(io.sockets.adapter.rooms[100]);
 
     //console.log(io.nsps['/'].adapter.rooms['100'].length);
-    io.sockets.to('100').emit('playerJoinedRoom', data);
 
    // io.sockets.emit('playerJoinedRoom', data);
     /*if( room != undefined ){
@@ -61,4 +76,8 @@ function playerJoinGame(data) {
         // Otherwise, send an error message back to the player.
         this.emit('error',{message: "This room does not exist."} );
     }*/
+};
+
+function roomInfo(roomId) {
+    io.sockets.to(roomId).emit('getRoomInfo', rooms[roomId].players);
 };
